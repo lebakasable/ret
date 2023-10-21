293285af93655c44f9398c4cfd78b1e4d3113960
Frexs <frexs.dev@gmail.com>
2023-10-21 21:30:35 +0200

    feat: tokenize 'delete' command

--- src/command.rs
+++ src/command.rs
@@ -37,7 +37,10 @@ pub enum Command {
         keyword: Token
     },
     Quit,
-    DeleteRule(Loc, Token),
+    DeleteRule {
+        keyword: Token,
+        name: Token,
+    },
     Load(Token),
     Save(Token),
     List,
@@ -114,7 +117,7 @@ impl Command {
                 let name = lexer.expect_token(TokenKind::Ident).map_err(|(expected_kind, actual_token)| {
                     diag.report(&actual_token.loc, Severity::Error, &format!("`delete` command expects {expected_kind} as an argument but got {actual_token} instead", actual_token = actual_token.report()));
                 }).ok()?;
-                Some(Command::DeleteRule(keyword.loc, name))
+                Some(Command::DeleteRule { keyword, name })
             }
             _ => {
                 let expr = Expr::parse(lexer, diag)?;
@@ -739,16 +742,16 @@ impl Context {
                     return None;
                 }
             },
-            Command::DeleteRule(loc, name) => {
+            Command::DeleteRule { keyword, name } => {
                 if delete_item_by_key(&mut self.rules, &name) {
                     diag.report(
-                        &loc,
+                        &keyword.loc,
                         Severity::Info,
                         &format!("rule `{}` has been removed", name.text),
                     );
                 } else {
                     diag.report(
-                        &loc,
+                        &keyword.loc,
                         Severity::Error,
                         &format!("rule `{}` does not exist", name.text),
                     );
